6ee55501335b27d612dcbfef85981e23d14796b2
HuuBaa <742790905@qq.com>
2018-05-07 19:46:14 +0800

    2-7

--- test2/test2/t2.cpp
+++ test2/test2/t2.cpp
@@ -97,41 +97,43 @@ int main()
 	}*/
 
 
-	//杨辉三角
-	int a[N][N] = { 0 };
-	int n;
-	printf("请输入阶数！");
-	scanf_s("%d", &n);
+	////杨辉三角
+	//int a[N][N] = { 0 };
+	//int n;
+	//printf("请输入阶数！");
+	//scanf_s("%d", &n);
 
-	for (int i = 0; i < n; i++)
-		a[i][0] = 1;
+	//for (int i = 0; i < n; i++)
+	//	a[i][0] = 1;
 
-	for (int i = 0; i < n; i++)
-		a[i][i] = 1;
+	//for (int i = 0; i < n; i++)
+	//	a[i][i] = 1;
 
-	for (int i = 2; i < n; i++)
-	{
-		for (int j = 1; j < i; j++)
-		{
-			a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
-		}
-	}
+	//for (int i = 2; i < n; i++)
+	//{
+	//	for (int j = 1; j < i; j++)
+	//	{
+	//		a[i][j] = a[i - 1][j] + a[i - 1][j - 1];
+	//	}
+	//}
 
-	for (int i = 0; i < n; i++)
-	{	
-		
-		for (int j = 0; j <n-i; j++)
-		{
-			printf("%3c", SPACE);
-		}
+	//for (int i = 0; i < n; i++)
+	//{	
+	//	
+	//	for (int j = 0; j <n-i; j++)
+	//	{
+	//		printf("%3c", SPACE);
+	//	}
 
 
-		for (int j = 0; j <= i; j++)
-		{
-			printf("%6d", a[i][j]);
-		}
-		printf("\n");
-	}
+	//	for (int j = 0; j <= i; j++)
+	//	{
+	//		printf("%6d", a[i][j]);
+	//	}
+	//	printf("\n");
+	//}
+
+
 
 
 	system("pause");
